881b27abaf919c6d5ad6bd70ca5e27f566707420
Frantisek B <frantisekbabinsky.cz@gmail.com>
2026-07-05 01:16:06 +0200

    feat(profile): support profile statistics

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,7 @@ const UserSchema = new Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 export default models.User || mongoose.model("User", UserSchema);
