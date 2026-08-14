5a09f11594c522e8ecea0d712bb02e1fb703b049
Frantisek <frantisekbabinsky.cz@gmail.com>
2026-08-14 15:02:45 +0200

    feat(playlists): populate playlist soundtracks

--- src/app/api/playlists/[id]/route.ts
+++ src/app/api/playlists/[id]/route.ts
@@ -64,7 +64,7 @@ export async function GET(
     const playlist = await Playlist.findOne({
       _id: id,
       userId,
-    });
+    }).populate("soundtracks");
 
     if (!playlist) {
       return NextResponse.json(
